f3e92124faa55d30d98a4f658aff4f19b166d58e
YoofiHonny0405 <yoofi.honny1@gmail.com>
2025-02-02 19:11:11 -0600

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -4,38 +4,44 @@
 #include <filesystem>
 #include <cstdlib>
 #include <sstream>
+#include <unordered_set>
+#include <unistd.h>
 
-namespace fs =std::filesystem;
+namespace fs = std::filesystem;
 
-std::vector<std::string> split(const std::string& str, char delimiter) {
-    std::vector<std::string> tokens;
-    std::stringstream ss(str);
-    std::string token;
-    while (std::getline(ss, token, delimiter)) {
-        if (!token.empty()) {
-            tokens.push_back(token);
-        }
-    }
-    return tokens;
+std::vector<std::string> split(const std::string& str, char delimiter){
+   std::vector<std::string> tokens;
+   std::stringsstream ss(str);
+   std::string token;
+
+   while (std::getlines(ss, token, delimiter)){
+      if(!token.empty()){
+        tokens.push_back(token);
+      }
+   }
+   return tokens;
 }
-std::string findExecutable(const std::string& command) {
-    const char* pathEnv = std::getenv("PATH");
-    if (!pathEnv) return "";
 
-    std::vector<std::string> paths = split(pathEnv, ':');
-    for (const std::string& dir : paths) {
-        fs::path filePath = fs::path(dir) / command;
-        if (fs::exists(filePath) && fs::is_regular_file(filePath) && access(filePath.c_str(), X_OK) == 0) {
-            return filePath.string();
-        }
+std::string findExecutable(const std::string& command){
+  const char* pathenv =std::getenv("PATH");
+  if(!pathEnv) return "";
+
+  std::vector<std::string> paths = split(pathEnv, ':');
+  for(const std::string& dir : paths){
+    fs::path filePath = fs::path(dir)/command;
+    if(fs::exists(filePath) && fs::is_regular_file(filepath) && access(filePath.c_str(), X_OK) == 0){
+      return filepath.string();
     }
-    return "";
+  }
+  return "";
 }
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
+  std::unordered_set<std::string> builtins = {"echo", "exit","type"};
   // Uncomment this block to pass the first stage
    
    
@@ -48,23 +54,23 @@ while (true)
 
   if(input.empty()) continue;
 
-   
   if (input.rfind("type", 0) == 0){
-      // std::string command = input.substr(5);
-      if(command == "echo" || command =="exit" || command == "type" ){
+       std::string command = input.substr(5);
+      
+      if(builtins.count(command)){
         std::cout << command << " is a shell builtin"<< std::endl;
-      }else{
-        std::string execPath = findExecutable(command);
-        if(!execPath.empty()){
-          std::cout << command << " is " << execPath << std::endl;
-        }else{
-          std::cout << command << " : not found" << std::endl;
-        }
       }
-  }
+      else {
+       std::string execPath =findExecutable(command);
+       if(!execPath.empty()){
+          std::cout << command << " is " << execPath << std::endl;
+       }else{
+          std::cout << command << ": not found"<<std::endl;
+       }
+    }
   
   } 
-  else if (input.rfind("echo ", 0) == 0) {
+  if (input.rfind("echo ", 0) == 0) {
             std::cout << input.substr(5) << std::endl;
   } else {
        std::cout << input << ": command not found" << std::endl;
