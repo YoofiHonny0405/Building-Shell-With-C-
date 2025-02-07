232778c2697f1bc65662169312160f4713ee6fd1
YoofiHonny0405 <yoofi.honny1@gmail.com>
2025-02-06 22:42:59 -0600

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -191,16 +191,19 @@ while (true)
         if (i > 1) output += " ";
         std::string arg = args[i];
         
+        // Remove surrounding quotes if present
+        if (arg.size() >= 2 && 
+            ((arg.front() == '"' && arg.back() == '"') || 
+             (arg.front() == '\'' && arg.back() == '\''))) {
+            arg = arg.substr(1, arg.size() - 2);
+        }
+        
+        // Preserve escaped characters
         std::string processed;
         for (size_t j = 0; j < arg.length(); ++j) {
             if (arg[j] == '\\' && j + 1 < arg.length()) {
-                char next = arg[j + 1];
-                if (next == '\'' || next == '"') {
-                    processed += next;
-                    ++j;
-                } else {
-                    processed += arg[j];
-                }
+                processed += arg[j];
+                processed += arg[++j];
             } else {
                 processed += arg[j];
             }
@@ -210,7 +213,6 @@ while (true)
     }
     std::cout << output << std::endl;
 }
-
 
  else {
       pid_t pid = fork();
